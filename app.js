28794773e2a5ad59c6ce431c08ae45190c8cb533
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-16 18:49:09 +0200

    modification du fichier app et ajout du CRUD dans client.js

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 const express = require("express"); //on importe express
 const home = require("./routes/home"); //on inclut le fichier home.js
+const client = require("./routes/client");
 const app = express();
 
-app.use(home)//on invoque l'instance app
-
-//Route et middleware
-// Opérations CRUD sur les clients
-app.get("/client/voir",(req,res)=>{
-    console.log("Voir");
-    res.send("<h1>Voir les informations du client</h1>");
-})
-
-app.post("/client/ajouter",(req,res)=>{
-    console.log("Ajouter");
-    res.send("<h1>Ajouter un nouveau client</h1>");
-})
-
-app.put("/client/modifier",(req,res)=>{
-    console.log("Modifier");
-    res.send("<h1>Modifier un client</h1>");
-})
-
-app.delete("/client/supprimer",(req,res)=>{
-    console.log("Supprimer");
-    res.send("<h1>Supprimer un client</h1>");
-})
+app.use(home);//on invoque l'instance app
+app.use(client);
 
 app.listen(3000,()=>{
     console.log("Serveur en écoute...");
---
+++ routes/client.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+router = express.Router();
+
+// Opérations CRUD sur les clients
+router.get("/client/voir",(req,res)=>{
+    console.log("Voir");
+    res.send("<h1>Voir les informations du client</h1>");
+})
+
+router.post("/client/ajouter",(req,res)=>{
+    console.log("Ajouter");
+    res.send("<h1>Ajouter un nouveau client</h1>");
+})
+
+router.put("/client/modifier",(req,res)=>{
+    console.log("Modifier");
+    res.send("<h1>Modifier un client</h1>");
+})
+
+router.delete("/client/supprimer",(req,res)=>{
+    console.log("Supprimer");
+    res.send("<h1>Supprimer un client</h1>");
+})
+
+module.exports = router;
