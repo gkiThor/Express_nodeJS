055a71ebe8e44261121253c305a1de59ef99ec6c
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-16 18:04:50 +0200

    ajout des routes et middleware: contact, envoyer, modifier, supprimer

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Test nodemon");
+const express = require("express"); //on importe express
+
+const app = express();
+
+
+//Route et middleware
+app.get("/",(req,res)=>{
+    console.log("Get");
+    res.send("GET");
+})
+
+app.get("/contact",(req,res)=>{
+    console.log("Contact");
+    res.send("Contact");
+})
+
+app.post("/envoyer",(req,res)=>{
+    console.log("envoyer");
+    res.send("envoyer");
+})
+
+app.put("/modifier",(req,res)=>{
+    console.log("Modifier");
+    res.send("Modifier");
+})
+
+app.delete("/supprimer",(req,res)=>{
+    console.log("Supprimer");
+    res.send("Supprimer");
+})
+
+app.listen(3000,()=>{
+    console.log("Serveur en écoute...");
+})
+
