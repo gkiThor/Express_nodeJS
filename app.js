2545f43804a521906f7589e555f63aebe34a9c26
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-17 15:15:02 +0200

    test de methodes et reponses client

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const app = express();
 app.use(home);//on invoque l'instance app
 app.use("/client",client);//Module client est prefixe par /client
 
+
+app.get("/test",(req,res)=>{
+    //res.send("<h1>Titre H1</1>");
+
+    // console.log("message de la console");
+    // res.end();
+
+    // res.download(__dirname + "/routes/home.js");
+
+    // res.json("{'nom':'TestNom'}");
+    // ob = {
+    //     nom:'Kiki',
+    //     prenom:'Guigui'
+    // }
+    // res.json(ob);
+
+    // res.redirect(301,"https://www.youtube.com/");//301 est une redirection permanente
+
+    res.sendFile(__dirname+"/index.html");
+})
+
+// app.all(/.*/,(req,res)=>{
+//     res.status(404).send("<h2>Page introuvable</h2>");
+// });//Toutes les méthodes sont éligibles
+
 app.listen(3000,()=>{
     console.log("Serveur en écoute...");
 })
